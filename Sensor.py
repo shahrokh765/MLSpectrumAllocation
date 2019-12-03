892b830af3afabadb643723bc08a4210cc93711d
caitaozhan <caitaozhan@gmail.com>
2019-12-02 21:49:47 -0500

    fix environment issue on Caitao-desktop

--- Sensor.py
+++ Sensor.py
@@ -3,3 +3,7 @@ from Commons.Point import Point
 class Sensor:
     def __init__(self, loc:Point, cost: float, std: float):
         self.loc, self.rp, self.cost, self.std = loc, -float('inf'), cost, std
+
+if __name__ == '__main__':
+    p = Point(0, 1)
+    print(p.get_cartesian())
